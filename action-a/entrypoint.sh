ebd06afeffd521c0662273b1256743f050bf9ba1
Alexander Nurzhanov <Alexander.Nurzhanov@md.de>
2020-06-10 08:35:58 +0200

    added new icon

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello $INPUT_MY_NAME! \nWelcome to your first action!\nIf you can read this Message, it means that your settings are correct."
+sh -c "echo Hello $INPUT_MY_NAME! Welcome to your first action! If you can read this Message, it means that your settings are correct."
